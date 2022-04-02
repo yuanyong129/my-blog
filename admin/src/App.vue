794a0d805696b9492c11989bff240125df94a6b6
yuany <yuanyong129@outlook.com>
2022-04-02 20:37:28 +0800

    feat: 完成登录页样式

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -13,13 +13,13 @@ import { NMessageProvider } from 'naive-ui'
   
 </template>
 
-<style>
+<style lang="scss">
+@import '@/styles/index.scss';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* text-align: center; */
+  /* color: #2c3e50; */
 }
 </style>
--- admin/src/views/auth/Login/index.vue
+++ admin/src/views/auth/Login/index.vue
@@ -2,39 +2,42 @@
 import { ref } from 'vue'
 import { NForm, NFormItem, NInput, NButton, FormInst, useMessage } from 'naive-ui'
 
-const formValue = {
+const loginForm = ref({
   username: '',
   password: ''
-}
+})
+
 const formRef = ref<FormInst | null>(null)
 const rules = {}
 const message = useMessage()
 const handleValidateClick = () => {
-  
-  console.log(message)
+  console.log(loginForm.value)
   message.success('success')
 }
 </script>
 
 <template>
-<n-form
+<div style="display: flex; justify-content: center; align-items: center; width: 100%; height: 100vh;">
+  <n-form
+    class="fluent-card"
+    style="width: 500px; padding: 30px; box-sizing: border-box;"
     ref="formRef"
-    :label-width="80"
-    :model="formValue"
+    :model="loginForm"
     :rules="rules"
     size="medium"
   >
-    <n-form-item label="姓名" path="username">
-      <n-input v-model:value="formValue.username" placeholder="输入姓名" />
+    <div style="text-align: center; padding: 10px; font-size: 24px;">空澄博客管理系统</div>
+    <n-form-item label="用户名" path="username">
+      <n-input v-model:value="loginForm.username" placeholder="请输入用户名" />
     </n-form-item>
-    <n-form-item label="年龄" path="password">
-      <n-input v-model:value="formValue.password" placeholder="输入年龄" />
+    <n-form-item label="密码" path="password">
+      <n-input v-model:value="loginForm.password" placeholder="请输入密码" />
     </n-form-item>
-    <n-form-item>
-      <n-button attr-type="button" @click="handleValidateClick">
-        验证
+    <div style="text-align: right;">
+      <n-button type="primary" secondary attr-type="button" @click="handleValidateClick">
+        登录
       </n-button>
-    </n-form-item>
+    </div>
   </n-form>
-  
+</div>
 </template>
